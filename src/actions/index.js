312e53cd21de639215d1875a52c5a2c91352edbe
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-01 18:47:05 +0100

    Add actions to index.js

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export const ADD_POST = 'ADD_POST'
+export const ADD_COMMENT = 'ADD_COMMENT'
+export const UPDATE_POST = 'UPDATE_POST'
+export const UPDATE_COMMENT = 'UPDATE_COMMENT'
+export const DELETE_POST = 'DELETE_POST'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
+export const VOTE_POST = 'VOTE_POST'
+export const VOTE_COMMENT = 'VOTE_COMMENT'
