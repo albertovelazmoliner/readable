c14247cce77b7b2827cf3880f264f2af91b66f46
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-04 23:52:38 +0100

    Add logger middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd(action.type)
+  return result
+}
 
 ReactDOM.render(
   <BrowserRouter>
