a81749507a654fcde39ed2211a8ac0dc7fb86692
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-04 23:53:15 +0100

    Add store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import categories from './reducers'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 const logger = store => next => action => {
   console.group(action.type)
   console.info('dispatching', action)
@@ -12,6 +17,18 @@ const logger = store => next => action => {
   console.groupEnd(action.type)
   return result
 }
+
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  categories,
+  composeEnhacers(
+    applyMiddleware(
+      thunk,
+      logger
+    )
+  )  
+)
 
 ReactDOM.render(
   <BrowserRouter>
