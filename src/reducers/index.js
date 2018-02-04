8023afad84a8da0db2e47aafae9e98bd0d0d8986
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-04 23:53:44 +0100

    Add categories reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux'
+import {
+  REQUEST_CATEGORIES,
+  RECEIVE_CATEGORIES
+} from './../actions'
+ 
+const categories = (
+  state = {
+    categories: []
+  },
+  action
+) => {
+  switch (action.type) {
+    case RECEIVE_CATEGORIES:
+      return {
+        ...state,
+        categories: action.categories
+      }
+    default: 
+      return state
+  }
+}
+
+export default categories
