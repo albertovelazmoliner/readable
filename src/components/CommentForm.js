a4d90c1798c9526284d1255c504263a7e6af62f8
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-10 16:03:37 +0100

    Add loading state for saving comment process

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -9,23 +9,31 @@ class BaseForm extends Component {
     loading: false
   }
 
-
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        this.setState({ loading: true })
+        this.props.onOk(values)
+      }
+    });
+  }
 
   render() {
     
     const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
     return (
       <div>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
         <Modal
           title="Create a new comment"
           visible={this.props.visible}
           footer={[
             <Button key="cancel" onClick={this.props.onCancel}>{this.props.cancelText}</Button>,
-            <Button key="submit" type="primary" loading={this.state.loading} 
-              // onClick={() => this.props.onOk({})}
-              onClick={() => console.log(this)}>
-              {this.props.okText}
+            <Button key="submit" type="primary" 
+              loading={this.state.loading}
+              onClick={this.handleSubmit}>
+                {this.props.okText}
             </Button>,
           ]}
         >
