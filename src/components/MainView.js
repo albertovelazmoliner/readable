bc5b2c08dda06f0b414ff062a3785812369a2a6a
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-18 18:15:23 +0100

    Fix UI of MainView and Add vote functionality to Post on MainView

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './MainView.css'
-import { Affix, Select, Icon, Row, Col, List, Button, Layout } from 'antd'
+import { Affix, Select, Icon, Row, Col, List, Button, Layout, Card } from 'antd'
+import moment from 'moment'
 import 'antd/dist/antd.css' 
 import { connect } from 'react-redux'
-import { fetchCategories, fetchAllPosts } from '../actions'
+import { fetchCategories, fetchAllPosts, postVotePost } from '../actions'
 
 const { Header, Footer, Content } = Layout;
 
 class MainView extends Component {
+  
   componentDidMount() {
     if (this.props.categories.length === 0) {
       this.props.getCategories()
     }
     this.props.getAllPosts()
   }
+
+  handlePostVote = (postId, option = 'upVote') => {
+    this.props.sendPostVote(postId, option)
+      .then(() => {})
+      .catch(error => console.log(error))
+  }
+
+
 
   render() {
     const Option = Select.Option
@@ -26,13 +36,13 @@ class MainView extends Component {
     return (
       <div>
         <Layout>
-          <Header>Readable Project</Header> 
-
           <Content>
           <Row>
-              <Col span={12}>
+              <Col span={1}/>
+              <Col span={10}>
                 <h3 style={{ margin: '16px 0' }}>Categories</h3>
                 <List
+                  className="list-item"
                   bordered
                   dataSource={this.props.categories}
                   renderItem= {
@@ -42,7 +52,9 @@ class MainView extends Component {
                   }
                 />
               </Col>
-              <Col span={12}>
+              <Col span={1}/>
+              <Col span={1}/>
+              <Col span={10}>
               <Select defaultValue="timestamp" style={{ width: 120 }} onChange={handleChange}>
                 <Option value="vote">By vote score</Option>
                 <Option value="timestamp">By date</Option>
@@ -52,21 +64,42 @@ class MainView extends Component {
                 <List
                   className="categories-list"
                   bordered
+                  itemLayout="vertical"
                   dataSource={Object.keys(this.props.posts).map(key => this.props.posts[key])}
                   renderItem= {
-                    post => (<List.Item key={post.id}>
-                      <Link to={'/post/' + post.id}><strong>{post.title}</strong></Link> <br/> {post.author} <br/>
-                      {post.body}
-                      </List.Item>)
+                    post => (
+                      <List.Item key={post.id}
+                        className="list-item"
+                        actions={[
+                          <Icon type="like" onClick={() => this.handlePostVote(post.id)}/>, 
+                          <Icon type="dislike" onClick={() => this.handlePostVote(post.id, "downVote")}/>
+                        ]}>
+                      
+                        <List.Item.Meta
+                          title={
+                            <Link to={'/post/' + post.id}>
+                              {post.title}
+                            </Link>
+                          }
+                        />      
+                        <h4><p>Author: {post.author}</p></h4>
+                        <h3><p>{post.body}</p></h3>
+                        <p>Votes: {post.voteScore}</p>
+                        <p>{moment(post.timestamp).format('LLL')}</p>  
+                      </List.Item>
+                    )
                   }
                 />
               </Col>
+              <Col span={1}/>
             </Row>
           </Content>
           <Footer>
-            {/* <Button type="primary" icon="plus" size="large"> */}
-            <Link to="/PostForm">Create a new Post</Link>
-            {/* </Button> */}
+            <Link to="/PostForm">
+              <Button type="primary" icon="plus" size="large">
+                Create a new Post
+              </Button>
+            </Link>
           </Footer>
         </Layout>
         <div>
@@ -87,7 +120,8 @@ function mapStateToProps (state) {
 function mapDispatchToProps (dispatch) {
   return {
     getCategories: () => dispatch(fetchCategories()),
-    getAllPosts: () => dispatch(fetchAllPosts())
+    getAllPosts: () => dispatch(fetchAllPosts()),
+    sendPostVote: (postId, voteOption) => dispatch(postVotePost(postId, voteOption)),
   }
 }
 
