a2b37a8d7ed29a52f29c50cb21abd5e27417ef3d
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-23 18:16:38 +0100

    Add error handler to all api calls and show an error message to user

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,6 @@
-const api = 'http://localhost:3001';
+import { message } from 'antd'
+
+const api = 'http://localhost:3001'
 
 // Generate a unique token for storing your data on the backend server.
 let token = window.localStorage.token
@@ -11,39 +13,43 @@ const headers = {
   'Authorization': token
 }
 
-const myFetch = (url, options) => fetch(url, options).catch(err => {
-  // show toast
-
-  
-  throw err
-})
+const errorHandler = msg => (error) => {
+  message.error(`There was an error: ${msg}`)
+  console.error(error)
+  throw error
+}
 
 export const getCategories = () =>
   fetch(`${api}/categories`, { headers })
     .then(res => res.json())
     .then(data => data.categories)
-    .catch((err) => console.log("Error => " + err))
+    .catch(errorHandler('failed getting categories'))
 
 export const getPostFromCategory = (category) =>
   fetch(`${api}/${category}/posts`, { headers })
     .then(res => res.json())
+    .catch(errorHandler('failed getting posts'))
 
 export const getAllPosts = () => 
   fetch(`${api}/posts`, { headers })
     .then(res => res.json())
+    .catch(errorHandler('failed getting posts'))
 
 export const getPost = (postId) =>
   fetch(`${api}/posts/${postId}`, { headers })
     .then(res => res.json())
+    .catch(errorHandler('failed getting post'))
 
 export const getAllComments = (postId) =>
   fetch(`${api}/posts/${postId}/comments`, { headers })
     .then(res => res.json())
+    .catch(errorHandler('failed getting comments'))
 
 export const getComment = (commentId) =>
   fetch(`${api}/comments/${commentId}`, { headers })
     .then(res => res.json())
     .then(data => data)
+    .catch(errorHandler('failed getting comment'))
 
 export const voteComment = (commentId, option) => 
   fetch(`${api}/comments/${commentId}`, {
@@ -54,6 +60,7 @@ export const voteComment = (commentId, option) =>
     },
     body: JSON.stringify({ option })
   }).then(res => res.json())
+  .catch(errorHandler('failed voting comment'))
 
 export const postPost = (post) => 
   fetch(`${api}/posts`, {
@@ -64,6 +71,7 @@ export const postPost = (post) =>
     },
     body: JSON.stringify(post)
   }).then(res => res.json())
+  .catch(errorHandler('failed saving post'))
 
 export const votePost = (postId, option = "upVote") =>
   fetch(`${api}/posts/${postId}`, {
@@ -74,7 +82,7 @@ export const votePost = (postId, option = "upVote") =>
     },
     body: JSON.stringify({ option })
   }).then(res => res.json())
-  .catch((err) => console.log("Error => " + err))
+  .catch(errorHandler('failed voting post'))
 
 export const updatePost = (postId, author, title, body, category) =>
   fetch(`${api}/posts/${postId}`, {
@@ -85,6 +93,7 @@ export const updatePost = (postId, author, title, body, category) =>
     },
     body: JSON.stringify({ author, title, body, category })
   }).then(res => res.json())
+  .catch(errorHandler('failed updating post'))
     
 export const deletePost = (postId) =>
   fetch(`${api}/posts/${postId}`, {
@@ -94,6 +103,7 @@ export const deletePost = (postId) =>
       'Content-Type' : 'application/json'
     }
   }).then(res => res.json())
+  .catch(errorHandler('failed deleting post'))
 
 export const postComment = (comment) =>
   fetch(`${api}/comments`, {
@@ -104,6 +114,7 @@ export const postComment = (comment) =>
     },
     body: JSON.stringify(comment)
   }).then(res => res.json())
+  .catch(errorHandler('failed saving comment'))
 
 export const deleteComment = (commentId) =>
   fetch(`${api}/comments/${commentId}`, {
@@ -113,6 +124,7 @@ export const deleteComment = (commentId) =>
       'Content-Type' : 'application/json'
     }
   }).then(res => res.json())
+  .catch(errorHandler('failed deleting comment'))
 
 export const updateComment = (commentId, body, timestamp = Date.now()) => 
   fetch(`${api}/comments/${commentId}`, {
@@ -123,3 +135,4 @@ export const updateComment = (commentId, body, timestamp = Date.now()) =>
     },
     body: JSON.stringify({ body, timestamp })
   }).then(res => res.json())
+  .catch(errorHandler('failed updating comment'))
