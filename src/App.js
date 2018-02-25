76564499b42fffe9c8131433a41aba938c4ff7e5
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-25 16:32:35 +0100

    Add Switch and NotFound route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Router, Switch } from 'react-router-dom'
 import MainView from './components/MainView'
 import PostForm from './components/PostForm'
 import Post from './components/Post'
+import NotFound from './components/NotFound'
 
 class ReadableApp extends React.Component {
   render() {
     return (
-      <div className="app">
-        <Route exact path="/posts/edit/:post_id" component={PostForm} />
-        <Route exact path="/posts/post/new" component={PostForm} />
-        <Route exact path="/" component={MainView} />
-        <Route exact path="/:category" component={MainView} />
-        <Route exact path="/:category/:post_id" component={Post} />
-      </div>
+        <div className="app">
+          <Switch>
+            <Route exact path="/posts/edit/:post_id" component={PostForm} />
+            <Route exact path="/posts/post/new" component={PostForm} />
+            <Route exact path="/" component={MainView} />
+            <Route exact path="/:category" component={MainView} />
+            <Route exact path="/:category/:post_id" component={Post} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
     )
   }
 }
