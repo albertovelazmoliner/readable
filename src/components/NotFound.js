4282cf692c9c25ef55de13afde7d21977c9e43de
Alberto Velaz <albertovelaz.dev@gmail.com>
2018-02-25 16:57:45 +0100

    Add UI to the NotFound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Card, Icon, Button } from 'antd'
 
 const NotFound = (props) => {
   return (
-    <div>
-      Not Found
+    <div style={{padding:"100px"}}>
+      <Card 
+        style={{ width:"50%", margin:"auto"}}
+        title="Something's wrong here..."
+        actions={[
+          <Button href="mailto:albertovelaz@gmail.com">
+            Contact with us
+          </Button>,
+          <Button type="primary" icon="home"
+          onClick={() => window.location.replace('/')}>Home</Button>
+        ]}
+      >
+        <h1>404</h1>
+        <p>We can't find the page you're looking for. Contact with us or head back to home</p>
+      </Card>
     </div>
   )
 }
